ec622d97e2f8ce9abe9435f69056df2363731a15
kievkevin13 <kievkevin13@gmail.com>
2020-02-19 21:56:07 -0600

    Login

--- Kevin Flores 9959-18-17632/Login/src/Seguridad.java
+++ Kevin Flores 9959-18-17632/Login/src/Seguridad.java
@@ -1,18 +1,22 @@
 
 import javax.swing.JOptionPane;
 
+
 public class Seguridad {
+    frmLogin login=new frmLogin();
    String strRespuesta;
     public void ValidarUsuarios(String Usuario[],String strUsuario, String strContraseña, int Intentos){
         boolean blEncontrado=false;
-        
+      
         for(int i=0; i < Usuario.length;i++){  
         if ((Usuario[i].equalsIgnoreCase(strUsuario)&& Usuario[i+1].equals(strContraseña))){
              strRespuesta="Bienvenido "+strUsuario;
              blEncontrado=true;
              JOptionPane.showMessageDialog(null, strRespuesta, "Inicio de sesión", JOptionPane.INFORMATION_MESSAGE);
              Intentos=0;
-             break;
+             login.setIntentos(Intentos);
+                  break;
+        
             }
         }if (blEncontrado==false){
             strRespuesta= "Usuraio o contraseña no válidos van: "+ Intentos+ " intentos fallidos.";
